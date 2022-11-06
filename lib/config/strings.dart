cf0aef7399ade1d9d8c5e5c5fe8c055258c04bb4
PulkitAgga <devilmaestro123456789@gmail.com>
2022-11-06 11:48:45 +0530

    Card_UI

---
+++ lib/config/strings.dart
@@ -0,0 +1,8 @@
+List category = [
+  {"name": "Groceries", "amount": 500.0},
+  {"name": "Online Shopping", "amount": 2300.0},
+  {"name": "Eating Out", "amount": 1200.0},
+  {"name": "Bills", "amount": 5000.0},
+  {"name": "Subscriptions", "amount": 1000.0},
+  {"name": "Fees", "amount": 6000.0},
+];
